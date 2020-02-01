32831c73263fa6a4f5125deb73d175d19b282312
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-02-01 19:33:05 +0000

    feat: Working with Numbers, Strings & Booleans

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     // TS: we can only fail at the Development (witch allow us to fix bugs earlier)
     // JS: we can only fail at the Runtime
     // The key difference is: JavaScript uses "dynamic types" (resolved at runtime),
@@ -7,11 +7,18 @@ function add(n1: number, n2: number) {
     //     throw new Error('Incorrect input!')
     // }
 
-    return n1 + n2
+    const result = n1 + n2
+
+    if (showResult) {
+        console.log(phrase + result)
+    } else {
+        return n1 + n2
+    }
 }
 
 const number1 = 5
 const number2 = 2.8
+const printResult = true
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
