157d443e1d03ce8941aaa6e6e41f8fcda9ea2488
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-02-01 19:25:08 +0000

    feat: TypeScript Types vs JavaScript Types

--- app.ts
+++ app.ts
@@ -1,4 +1,12 @@
 function add(n1: number, n2: number) {
+    // TS: we can only fail at the Development (witch allow us to fix bugs earlier)
+    // JS: we can only fail at the Runtime
+    // The key difference is: JavaScript uses "dynamic types" (resolved at runtime),
+    // TypeScript uses "static types" (set during development).
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!')
+    // }
+
     return n1 + n2
 }
 
