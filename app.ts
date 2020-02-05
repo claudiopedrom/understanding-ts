2b2cfa1d234f7c13c89939af876290bad1e1a15e
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-02-05 22:11:22 +0000

    feat: Arrays Types

--- app.ts
+++ app.ts
@@ -5,7 +5,16 @@
 // } = {
 const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  // string[]
+  hobbies: ['Sports', 'Cooking']
 }
 
-console.log(person)
+let favoriteActivities: string[]
+favoriteActivities = ['Sports']
+
+console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
