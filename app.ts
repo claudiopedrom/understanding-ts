775e0a17a64b51b4f0dcf58414be6ab62627ef1d
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-02-09 22:27:08 +0000

    feat: Working with Enums

--- app.ts
+++ app.ts
@@ -3,18 +3,35 @@
 //   name: string
 //   age: number
 // } = {
-const person: {
-  name: string
-  age: number
-  hobbies: string[]
-  // Tuple
-  role: [number, string]
-} = {
+// const person: {
+//   name: string
+//   age: number
+//   hobbies: string[]
+//   // Tuple
+//   role: [number, string]
+// } = {
+//   name: 'Maximilian',
+//   age: 30,
+//   // string[]
+//   hobbies: ['Sports', 'Cooking'],
+//   role: [2, 'author']
+// }
+
+// const ADMIN = 0
+// const READ_ONLY = 1
+// const AUTHOR = 2
+
+enum Role {
+  ADMIN = 'ADMIN',
+  READ_ONLY = 100,
+  AUTHOR = 'AUTHOR'
+}
+
+const person = {
   name: 'Maximilian',
   age: 30,
-  // string[]
   hobbies: ['Sports', 'Cooking'],
-  role: [2, 'author']
+  role: Role.ADMIN
 }
 
 // person.role.push('admin')
@@ -29,4 +46,9 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase())
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('is author')
 }
