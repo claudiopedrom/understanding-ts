085c6dde01d8ba2e09623f571475e150ea76dfc3
Cláudio Mindouro <claudiopedrom@gmail.com>
2020-04-21 23:17:44 +0100

    feat: Functions as Types

--- app.ts
+++ app.ts
@@ -8,4 +8,12 @@ function printResult(num: number): void {
 
 printResult(add(5, 12))
 
+let combinedValues: (a: number, b: number) => number
+
+combinedValues = add
+// combinedValues = printResult
+// combinedValues = 5
+
+console.log(combinedValues(8, 8))
+
 // let someValue: undefined
